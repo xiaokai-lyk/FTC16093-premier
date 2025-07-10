c4538643a036be45fa17152e31f0e96e39149e4b
xiaokai-lyk <105791771+xiaokai-lyk@users.noreply.github.com>
2025-07-10 19:45:50 +0800

    fix localization

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -7,8 +7,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        PinpointConstants.forwardY = 1;
-        PinpointConstants.strafeX = -2.5;
+        PinpointConstants.forwardY = 5.66929134;
+        PinpointConstants.strafeX = -3.26771654;
         PinpointConstants.distanceUnit = DistanceUnit.INCH;
         PinpointConstants.hardwareMapName = "odo";
         PinpointConstants.useYawScalar = false;
